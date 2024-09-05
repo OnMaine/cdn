ceaf35a40769e8203f0c2158e02619201c065415
OnMaine <volodymyr.vasylenko@futurragroup.com>
2024-09-05 14:30:22 +0200

    fix/

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/admin/Git/cdn/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'test',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        {
+          name: 'viewport',
+          content:
+            'width=device-width, height=device-height,initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+        },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+    },
+    cdnURL: 'https://cdn.example.com',
+  },
+
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
