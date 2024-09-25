31ace832cb57bd258ca97df2af0aff6618d47c34
Volodymyr Vasylenko <50885106+OnMaine@users.noreply.github.com>
2024-09-25 13:18:08 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
         { name: 'format-detection', content: 'telephone=no' },
       ],
     },
-    cdnURL: 'https://cdn.example.com',
+    cdnURL: 'https://11111cdn.example.com',
   },
 
   compatibilityDate: '2024-04-03',
